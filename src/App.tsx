00af5619bb2f347a3938ac9127e564256777de8b
MARJORIE DUBUT <marjorie.dubut@gmail.com>
2024-10-22 16:18:05 +0200

    use props

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,21 @@ import PokemonCard from "./components/PokemonCard.tsx";
 function App() {
 	return (
 		<div>
-			<PokemonCard />
+			<PokemonCard pokemon={pokemonList[0]} />
 		</div>
 	);
 }
+
+const pokemonList = [
+	{
+		name: "Bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+
+	{
+		name: "Mew",
+	},
+];
 
 export default App;
